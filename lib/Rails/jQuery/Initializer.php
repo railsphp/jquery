d6a309a7557494f4cf614f425dbbf898170d9a41
Parziphal <blackmesa@ubuntu-server.com>
2013-10-02 14:24:07 -0500

    fixed paths and autoloader

--- lib/Rails/jQuery/Initializer.php
+++ lib/Rails/jQuery/Initializer.php
@@ -5,7 +5,9 @@ class Initializer
 {
     public function initialize()
     {
-        \Rails::assets()->addPaths([realpath(__DIR__ . '/../../vendor/assets/javascripts')]);
+        \Rails::assets()->addPaths([
+            realpath(__DIR__ . '/../../../vendor/assets/javascripts')
+        ]);
         \Rails::assets()->addFilePatterns(['.map']);
     }
 }
